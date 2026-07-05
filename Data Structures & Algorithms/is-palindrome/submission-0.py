17089a20dd30c9528f2fd8570d29512226c09954
aajasim <64766142+aajasim@users.noreply.github.com>
2026-07-05 10:40:35 +0400

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s_mod = re.findall("[A-Za-z0-9]", s)
+        s_mod = [item.lower() if isinstance(item, str) else item for item in s_mod]
+        return s_mod == s_mod[::-1]
+        
